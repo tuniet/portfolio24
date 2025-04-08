5c3c09e692c3f260ab04880c3ea99a7783eb943d
tuniet <tofeju@gmail.com>
2025-04-08 15:31:25 +0200

    changed images to .webp format

--- src/Work.jsx
+++ src/Work.jsx
@@ -1,11 +1,11 @@
 import Card from './Card';
 import './Work.css';
-import meraki from './media/meraki.png'
-import tunietgames from './media/tuniet-games.png'
-import nobelnest from './media/nobelnest.png'
-import gymapp from './media/gymapp.png'
-import architecture from './media/architecture.png'
-import pomodoro from './media/pomodoro.png'
+import meraki from './media/meraki.webp'
+import tunietgames from './media/tuniet-games.webp'
+import nobelnest from './media/nobelnest.webp'
+import gymapp from './media/gymapp.webp'
+import architecture from './media/architecture.webp'
+import pomodoro from './media/pomodoro.webp'
 import reacti from './media/react.svg'
 import htmli from './media/html.svg'
 import cssi from './media/css.svg'
