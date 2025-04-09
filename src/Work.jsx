cd5dbebae1dc34715f1e05078d588d8baed15532
tuniet <tofeju@gmail.com>
2025-04-09 09:01:07 +0200

    neptunehotel project added

--- src/Work.jsx
+++ src/Work.jsx
@@ -21,7 +21,7 @@ function Work() {
         <div className="cards">
           <Card img={meraki} title={"MERAKI"} desc={"Meraki is a fictional clothing ecommerce"} roles={"Frontend Dev & UX/UI Designer"} tags={[htmli, cssi, reacti]} disable="" url="merakishop.netlify.app"/>
           <Card img={tunietgames} title={"TUNIET GAMES"} desc={"Tuniet games is a personal website where I publish small web games"} roles={"Frontend Dev & UX/UI Designer"} tags={[htmli, cssi, reacti]} disable="" url=""/>
-          <Card img={nobelnest} title={"NOBEL NEST"} desc={"Nobel Nest is a fictional hotel website"} roles={"Frontend Dev & UX/UI Designer"} tags={[htmli, cssi, reacti]} disable="" url=""/>
+          <Card img={nobelnest} title={"NEPTUNE HOTEL"} desc={"Neptune Hotel is a fictional hotel website"} roles={"Frontend Dev & UX/UI Designer"} tags={[htmli, cssi, reacti]} disable="" url="neptunehotel.netlify.app"/>
           <Card img={architecture} title={"WIP"} desc={"XXXXX is a architecture studio website"}  roles={"Frontend Dev & UX/UI Designer"} tags={[htmli]} disable={"disable"} url=""/>
           <Card img={gymapp} title={"WIP"} desc={"XXXXX is a web app to help people keep track of their progress in the gym"} roles={"Frontend Dev & UX/UI Designer"} tags={[htmli]} disable={"disable"} url=""/>
           <Card img={pomodoro} title={"WIP"} desc={"XXXXX is a web app to help people maintain focus during long sesions of study or coding, with a all in one place tool"} roles={"Frontend Dev & UX/UI Designer"} tags={[htmli]} disable={"disable"} url=""/>
